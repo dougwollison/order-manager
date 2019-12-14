26d93015b3496d66ebb74d0f03416b423fcf604d
Doug Wollison <doug@wollison.net>
2019-12-14 15:23:49 -0500

    add term_order handling on posts
    also start work on handling menu order handling on terms

--- includes/class-ordermanager-system.php
+++ includes/class-ordermanager-system.php
@@ -80,8 +80,66 @@ final class System extends Handler {
 	 * @since 1.0.0
 	 */
 	public static function register_hooks() {
+		// Query Manipulation
+		self::add_hook( 'get_terms_orderby', 'handle_term_order', 10, 3 );
+		self::add_hook( 'posts_orderby', 'handle_term_post_order', 10, 2 );
+
 		// Insert Manipulation
 		self::add_hook( 'create_term', 'default_term_menu_order', 10, 3 );
+	}
+
+	// =========================
+	// ! Query Manipulation
+	// =========================
+
+	/**
+	 * Filters the ORDERBY clause of the terms query.
+	 *
+	 * Adds handling of "menu_order" option.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string   $orderby    `ORDERBY` clause of the terms query.
+	 * @param array    $args       An array of term query arguments.
+	 * @param string[] $taxonomies An array of taxonomy names.
+	 *
+	 * @return string The filtered `ORDERBY` clause.
+	 */
+	public static function handle_term_order( $orderby, $args, $taxonomy ) {
+		return $orderby;
+	}
+
+	/**
+	 * Filters the ORDER BY clause of the query.
+	 *
+	 * Adds handling of "term_order" option.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string   $orderby The ORDER BY clause of the query.
+	 * @param WP_Query $query   The WP_Query instance (passed by reference).
+	 *
+	 * @return string The filtered `ORDERBY` clause.
+	 */
+	public static function handle_term_post_order( $orderby, $query ) {
+		global $wpdb;
+
+		if ( $query->get( 'orderby' ) == 'term_order' ) {
+			$tax_query = $query->tax_query;
+			// If for a single term in a single taxonomy, use post order
+			if ( count( $tax_query->queries ) == 1 && count( $tax_query->queries[0]['terms'] ) == 1
+			&& Registry::is_taxonomy_supported( $tax_query->queries[0]['taxonomy'], 'post_order_manager' )
+			&& ( $post_order = get_term_meta( $tax_query->queries[0]['terms'][0], 'post_order', true ) ) ) {
+				$post_order = array_map( 'absint', $post_order ); // ensure a list of numbers
+
+				$post_order = implode( ',', $post_order );
+				$order = $query->get( 'order' );
+
+				$orderby = "FIELD({$wpdb->posts}.ID,{$post_order}) {$order}";
+			}
+		}
+
+		return $orderby;
 	}
 
 	// =========================
