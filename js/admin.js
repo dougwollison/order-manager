f1b9370d2f8df340478f360e57ea70b5330b279c
Doug Wollison <doug@wollison.net>
2019-12-14 11:39:34 -0500

    rewrite to use nestedSortable when applicable

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,39 @@
 /* globals jQuery */
 jQuery( function( $ ) {
-	$( '.ordermanager-items' ).sortable( {
-		axis                 : false,
-		items                : 'li',
-		containment          : '.ordermanager-items',
-		cursor               : 'move',
-		forcePlaceholderSize : true,
-		revert               : true,
+	// Create the sortable options
+	var sortableOptions = {
+		cursor           : 'move',
+		handle           : '.ordermanager-item-label',
+		helper           : 'clone',
+		items            : 'li',
+		opacity          : 0.6,
+		placeholder      : 'ordermanager-placeholder',
+		revert           : true,
+		tolerance        : 'pointer',
+		toleranceElement : '> .ordermanager-item-label',
+	};
+
+	// Create the nestedSortable options
+	// a copy of the sortable options + an update event for the parent value
+	var nestedSortableOptions = $.extend( {}, sortableOptions, {
+		update: function( event, ui ) {
+			var $parent = ui.item.parents( '.ordermanager-item' ).first();
+			if ( $parent.length > 0 ) {
+				const parent_id = $parent.find( '.ordermanager-item-id' ).val();
+				ui.item.find( '> .ordermanager-item-parent' ).val( parent_id );
+			}
+		},
 	} );
+
+	// Apply the sortable options
+	// to order managers NOT using the is-nested class
+	$( '.ordermanager-interface:not(.is-nested)' )
+		.children( '.ordermanager-items' )
+		.sortable( sortableOptions );
+
+	// Apply the nestedSotrable options
+	// ONLY to order managers using the is-nested class
+	$( '.ordermanager-interface.is-nested' )
+		.children( '.ordermanager-items' )
+		.nestedSortable( nestedSortableOptions );
 } );
