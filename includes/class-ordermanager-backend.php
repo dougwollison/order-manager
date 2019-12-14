ac1f45cb40346e5dd9bc4faae5af4f2a922410a3
Doug Wollison <doug@wollison.net>
2019-12-13 21:51:15 -0500

    doing verify nonce wrong

--- includes/class-ordermanager-backend.php
+++ includes/class-ordermanager-backend.php
@@ -325,7 +325,7 @@ final class Backend extends Handler {
 		}
 
 		$post_type = $_POST['post_type'];
-		if ( ! wp_verify_nonce( $_POST[ "ordermanager_post_order:{$post_type}" ] ) ) {
+		if ( ! wp_verify_nonce( $_POST['_wpnonce'], "ordermanager_post_order:{$post_type}" ) ) {
 			cheatin();
 		}
 
@@ -355,7 +355,7 @@ final class Backend extends Handler {
 		}
 
 		$taxonomy = $_POST['taxonomy'];
-		if ( ! wp_verify_nonce( $_POST[ "ordermanager_term_order:{$taxonomy}" ] ) ) {
+		if ( ! wp_verify_nonce( $_POST['_wpnonce'], "ordermanager_term_order:{$taxonomy}" ) ) {
 			cheatin();
 		}
 
