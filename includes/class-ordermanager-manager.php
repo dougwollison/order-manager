111f6cff96e05e852a0f1f8fe0e2e1c6cc6a2780
Doug Wollison <doug@premise.ca>
2019-12-16 15:10:42 -0500

    whitespace tweak

--- includes/class-ordermanager-manager.php
+++ includes/class-ordermanager-manager.php
@@ -252,7 +252,7 @@ final class Manager extends Handler {
 	 */
 	public static function settings_page() {
 		global $plugin_page;
-?>
+		?>
 		<div class="wrap">
 			<h2><?php echo get_admin_page_title(); ?></h2>
 			<?php settings_errors(); ?>
